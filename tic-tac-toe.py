9b5c8e708423a80872f83cecd1038a9f63c0ece0
Alexander Berryhill <amsb99@gmail.com>
2022-01-13 10:53:05 -0700

    Updating to be Tic-tac-toe

---
+++ tic-tac-toe.py
@@ -0,0 +1,16 @@
+board = [[1,2,3],[4,5,6],[7,8,9]]
+
+def printBoard():
+    print(board[1][1]+"|"+board[1][2]+"|"+board[1][3])
+    print("-+-+-")
+    print(board[2][1]+"|"+board[2][2]+"|"+board[2][3])
+    print("-+-+-")
+    print(board[3][1]+"|"+board[3][2]+"|"+board[3][2])
+    print("-+-+-")
+    pass
+
+def playBoard():
+    pass
+
+def main():
+    printBoard()
