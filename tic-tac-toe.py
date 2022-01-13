fc1b3980cdcc3830f41c86d2f972c04928eb309f
Alexander Berryhill <amsb99@gmail.com>
2022-01-13 13:36:25 -0700

    Update tic-tac-toe.py
    Now there are turns, and changing board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,16 +1,44 @@
 board = [[1,2,3],[4,5,6],[7,8,9]]
+currentTurn = "X"
+gameActive = True
 
 def printBoard():
-    print(board[1][1]+"|"+board[1][2]+"|"+board[1][3])
+    print("{}|{}|{}".format(board[0][0],board[0][1],board[0][2]))
     print("-+-+-")
-    print(board[2][1]+"|"+board[2][2]+"|"+board[2][3])
+    print("{}|{}|{}".format(board[1][0],board[1][1],board[1][2]))
     print("-+-+-")
-    print(board[3][1]+"|"+board[3][2]+"|"+board[3][2])
-    print("-+-+-")
-    pass
+    print("{}|{}|{}".format(board[2][0],board[2][1],board[2][2]))
 
-def playBoard():
-    pass
+def getInput():
+    while True:
+        try:
+            print("\n{}'s turn to choose a square: ".format(currentTurn))
+            userInput = int(input())
+            break
+        except:
+            print("That is not a valid option!")
+    if userInput > 9 or userInput < 1:
+        print("That is not a valid option!")
+        userInput = getInput()
+    return userInput
+
+
+def playBoard(input):
+    boardSpot = board[int((input-0.1)/3)][(input+2)%3]
+    if boardSpot != "X" or boardSpot != "O":
+        board[int((input-0.1)/3)][(input+2)%3] = currentTurn
+
+def toggleTurn():
+    global currentTurn
+    if currentTurn == "X":
+        currentTurn = "O"
+    else:
+        currentTurn = "X"
 
 def main():
     printBoard()
+    playBoard(getInput())
+    toggleTurn()
+
+while(gameActive == True):
+    main()
